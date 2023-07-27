f94d1e71d7879c9c4b94c70154797baeac4204e6
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-07-27 12:26:35 +0300

    refactor style

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,11 +1,21 @@
+/** @jsxRuntime classic /
+/* @jsx jsx */
+/** @jsxImportSource @emotion/react */
+import { jsx } from "@emotion/react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { ISelectOption, IProjectProduct } from "../types/app.type";
 
+const classes = {
+  selectedButton: (isSelected: boolean) => ({
+    color: isSelected ? "#ffffff" : "#5316AE"
+  })
+};
 type Props = {
   onSelect: (value: IProjectProduct) => void;
   product: IProjectProduct;
 };
+
 const Switch = ({ onSelect, product }: Props) => {
   const options: ISelectOption<IProjectProduct>[] = [
     {
@@ -32,8 +42,8 @@ const Switch = ({ onSelect, product }: Props) => {
             key={option.value + index}
             disableRipple
             variant="text"
-            sx={{ color: product === option.value ? "#ffffff" : "#5316AE" }}
             onClick={() => onSelect(option.value)}
+            css={classes.selectedButton(product === option.value)}
           >
             {option.label}
           </Button>
